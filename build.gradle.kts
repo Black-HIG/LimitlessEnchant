044d90ea10959470ccb3478312b299e57bd2def4
BlackHIG <black-hig@outlook.com>
2024-07-21 15:37:57 +0800

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "cn.afsmc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
